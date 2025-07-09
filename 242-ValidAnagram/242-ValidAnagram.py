3768d96209f2b1d3def5ec3c4064a12758ae7aec
Pragnya Reddy Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-07-08 21:45:55 -0700

    File updated at 7/8/2025, 9:45:55 PM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,19 +1,19 @@
-# Last updated: 7/8/2025, 9:42:04 PM
+# Last updated: 7/8/2025, 9:45:55 PM
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        if len(s) != len(t):
+        if len(s) != len(t): # O(n)
             return False
         
         letter_hash = {}
 
-        for i in s:
+        for i in s: # O(n)
             if i not in letter_hash:
                 letter_hash[i] = 1
             else:
                 letter_hash[i] = letter_hash[i] + 1
 
-        for j in t:
+        for j in t: # O(n)
             if j in letter_hash:
                 if letter_hash[j] >= 1:
                     letter_hash[j] = letter_hash[j] - 1
@@ -22,8 +22,10 @@ class Solution:
             else:
                 return False
 
-        if all(not values for values in letter_hash.values()):
-            return True
-        else:
-            return False
+        # if all(not values for values in letter_hash.values()): # O(n)
+        #     return True
+        # else:
+        #     return False
+
+        return True
         
