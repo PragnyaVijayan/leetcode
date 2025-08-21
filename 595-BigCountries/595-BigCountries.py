f187a93ba099042b7660e6f0a86edd34a3e95b6c
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-08-21 12:25:24 -0700

    File updated at 8/21/2025, 12:25:24 PM

--- 595-BigCountries/595-BigCountries.py
+++ 595-BigCountries/595-BigCountries.py
@@ -1,10 +1,7 @@
-# Last updated: 8/21/2025, 12:20:33 PM
+# Last updated: 8/21/2025, 12:25:24 PM
 import pandas as pd
 
-def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    self_views = views[views['author_id'] == views['viewer_id']]
-    result = self_views.groupby(['author_id']).first().reset_index()
-    return result.rename(columns={'author_id': 'id'})[['id']]
-
-
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    invalid_tweets_df = tweets[tweets['content'].str.len() > 15]
+    return invalid_tweets_df[['tweet_id']]
     
