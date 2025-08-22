ff3e66e5d72746fd16d53af15ab218b96a2a178a
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-08-21 23:00:52 -0700

    File updated at 8/21/2025, 11:00:52 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,39 +1,70 @@
-# Last updated: 7/11/2025, 6:23:23 PM
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+# Last updated: 8/21/2025, 11:00:52 PM
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
 
         '''
-        - need to return number of unique elements in nums
-        - edge case:
-            - elements can repeat multiple times
+        - fast and slow pointer approach
+        - slow pointer will be before duplicates. can insert there
+        - if fast pointer reaches end of loop, everything between slow 
+            and fast pointer is '_'
 
-        psuedocode:
-        n = len(nums)
-        i = 0
-        #count_popped = 0
 
-        while i < n:
-            if nums[i] == nums[i+1]:
-                nums.pop(i+1)
-                #count_popped += 1
-            else:
-                i += 1
-        
-        return len(nums)
         '''
 
-        n = len(nums)
-        i = 0
-        #count_popped = 0
+        fast_pointer = 0
+        slow_pointer = 0
+        original_len = len(nums)
 
-        while i < len(nums) - 1:
-            # print(i)
-            # print(nums)
-            if nums[i] == nums[i+1]:
-                nums.pop(i+1)
-                #count_popped += 1
-            else:
-                i += 1
+        '''
+        1  1  2  2  3  4
+        ||
+
+        1  1  2  2  3  4
+        |  |
+
+        1  2  2  2  3  4
+           |  |
+
+        1  2  2  2  3  4
+           |     |
+
+        1  2  3  2  3  4
+              |     |
+
+        1  2  3  4  3  4
+                 |     |
+
+
+
+        '''
         
-        return len(nums)
+        #print(nums)
+
+        while fast_pointer < original_len - 1:
+            if nums[fast_pointer] != nums[fast_pointer + 1]:
+                slow_pointer += 1
+                nums[slow_pointer] = nums[fast_pointer + 1]
+
+            # print('nums :', nums)
+            # print('fast pointer: ', fast_pointer)
+            # print('slow pointer: ', slow_pointer)
+
+            fast_pointer += 1
+
+        if len(nums) > original_len:
+            for i in range(slow_pointer+1, original_len):
+                nums[i] = '-'
+            
+            for i in range(original_len+1, len(nums)):
+                nums.pop()
+
+        #print(slow_pointer + 1)
+        #print(nums)
+        return slow_pointer + 1
+        
+
         
