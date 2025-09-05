80a3509f298333f5591be8d3638b1d0f9ebf3f36
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-05 00:51:41 -0700

    File updated at 9/5/2025, 12:51:41 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,4 +1,4 @@
-# Last updated: 6/19/2025, 11:46:39 AM
+# Last updated: 9/5/2025, 12:51:41 AM
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -7,32 +7,10 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        '''
-        1) Do a nested for loop and add each and compare
-        2) Can try sorting, but kinda inefficient. Number can be 9 and need to add 8 and 1 from both
-        ends of the spectrum
-        3) If you have a number1, you need to find value - number1 to find 
-        number2. Number1 + number2 will be target
-        
-        '''
-
-        # for i in range(0, len(nums) - 1):
-        #     for j in range (i + 1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             print([i, j])
-        #             return([i, j])
-
-        # return None
-
-        nums_hash = set(nums)
-        #print(nums_hash)
-
-        for i in range(0, len(nums)):
-            number1 = nums[i]
-            number2 = target - number1
-            print(number1, number2)
-            if number2 in nums_hash and number1 in nums_hash:
-                if nums.index(number2) != i:
-                    return [i, nums.index(number2)]
-
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+                
         return None
+        
