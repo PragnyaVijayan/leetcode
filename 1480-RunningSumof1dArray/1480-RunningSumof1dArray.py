0900e466f050580d045b45cabe9cb37a61e92d1f
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-05 00:09:59 -0700

    File updated at 9/5/2025, 12:09:59 AM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
@@ -1,18 +1,20 @@
-# Last updated: 9/5/2025, 12:07:43 AM
+# Last updated: 9/5/2025, 12:09:59 AM
 class Solution(object):
     def runningSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
+        total=0
+        dummy=list(nums)
+        for i,j in enumerate(nums):
+           total=sum(nums[0:i+1])
+           dummy[i]=total
+           total=0
+        return dummy
 
-        running_sum = []
-        running_sum.append(0)
-
-        for num in nums:
-            running_sum.append(num + running_sum[-1])
         
-        del running_sum[0]
-        
-        return running_sum
+              
+            
+            
         
