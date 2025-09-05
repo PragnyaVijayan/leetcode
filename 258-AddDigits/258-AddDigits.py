4202de773a723029dd19f5595c5c788be267cbcc
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-05 00:01:45 -0700

    File updated at 9/5/2025, 12:01:45 AM

--- 258-AddDigits/258-AddDigits.py
+++ 258-AddDigits/258-AddDigits.py
@@ -1,11 +1,11 @@
-# Last updated: 9/5/2025, 12:00:21 AM
+# Last updated: 9/5/2025, 12:01:45 AM
 class Solution(object):
     def addDigits(self, num):
         """
         :type num: int
         :rtype: int
         """
-        if num < 10:
+        if len(str(num)) == 1:
             return num
         
         digit_sum = sum(int(d) for d in str(num))
