00975538c1e946a0466d2a2391e875425fe1bd36
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-06 22:45:42 -0700

    File updated at 9/6/2025, 10:45:41 PM

--- 398-RandomPickIndex/398-RandomPickIndex.py
+++ 398-RandomPickIndex/398-RandomPickIndex.py
@@ -1,27 +1,20 @@
-# Last updated: 9/6/2025, 10:43:55 PM
+# Last updated: 9/6/2025, 10:45:41 PM
 class Solution(object):
 
     def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
-        self.nums_hash = {}
-        for i in range(0, len(nums)):
-            if nums[i] not in self.nums_hash:
-                self.nums_hash[nums[i]] = [i]
-            else:
-                self.nums_hash[nums[i]].append(i)
+        self.freq = defaultdict(list)
+        self.nums = nums
+        
 
     def pick(self, target):
-        """
-        :type target: int
-        :rtype: int
-        """
-        import random
+        if target not in self.freq:
+            for i in range(len(self.nums)):
+                if self.nums[i] == target:
+                    self.freq[target].append(i)
 
-        all_indexes = list(self.nums_hash[target])
-        random_item = random.choice(all_indexes)
-        return random_item
+        return random.choice(self.freq[target])
+        
+
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
