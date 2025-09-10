a7916109e25c61b85771efb947ff2490fa2a4421
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-09 21:18:49 -0700

    File updated at 9/9/2025, 9:18:48 PM

--- 1672-RichestCustomerWealth/1672-RichestCustomerWealth.py
+++ 1672-RichestCustomerWealth/1672-RichestCustomerWealth.py
@@ -1,10 +1,35 @@
-# Last updated: 9/9/2025, 9:04:18 PM
+# Last updated: 9/9/2025, 9:18:48 PM
 class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        wealth = []
-        for account in accounts:
-            wealth.append(sum(account))
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        for op in operations:
+            
+            if op[0] == '-':
+                if op[1:].isdigit():
+                    record.append(op)
+                    
+            elif op[0] != '-' and op.isdigit():
+                record.append(op)
+
+            elif op == '+':
+                if len(record) >= 2:
+                    score1, score2 = record[-2:]
+                    record.append(int(score1) + int(score2))
+            
+            elif op == 'D':
+                if len(record) >= 1:
+                    record.append(2 * int(record[-1]))
+            
+
+            elif op == 'C':
+                if len(record) >= 1:
+                    record.pop()
         
-        return max(wealth)
+
+            print(record)
+            
+        print(record)
+
+        return sum([int(num) for num in record])
 
         
