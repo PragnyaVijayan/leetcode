ddd6268324d919527a8d64529d8926d6b00b83bd
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-15 23:52:40 -0700

    File updated at 9/15/2025, 11:52:40 PM

--- 3232-FindifDigitGameCanBeWon/3232-FindifDigitGameCanBeWon.py
+++ 3232-FindifDigitGameCanBeWon/3232-FindifDigitGameCanBeWon.py
@@ -1,21 +1,14 @@
-# Last updated: 9/15/2025, 11:50:32 PM
+# Last updated: 9/15/2025, 11:52:40 PM
 class Solution(object):
-    def canAliceWin(self, nums):
+    def checkIfPangram(self, sentence):
         """
-        :type nums: List[int]
+        :type sentence: str
         :rtype: bool
         """
 
-        sum_double_digits = 0
-        sum_single_digits = 0
-
-        for num in nums:
-            if 1 <= num <= 9:
-                sum_single_digits += num
-            elif 10 <= num <= 99:
-                sum_double_digits += num
-
-        #print(sum_single_digits, sum_double_digits)
-        return (sum_double_digits != sum_single_digits)
-
+        letters = set(sentence)
+        if len(letters) == 26:
+            return True
+        else:
+            return False
         
