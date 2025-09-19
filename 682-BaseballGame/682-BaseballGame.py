a7453dfa84d37c88d37c1ea777fdf1d11e361990
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-18 23:23:25 -0700

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.py
@@ -0,0 +1,35 @@
+# Last updated: 9/18/2025, 11:23:25 PM
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        for op in operations:
+            
+            if op[0] == '-':
+                if op[1:].isdigit():
+                    record.append(op)
+                    
+            elif op[0] != '-' and op.isdigit():
+                record.append(op)
+
+            elif op == '+':
+                if len(record) >= 2:
+                    score1, score2 = record[-2:]
+                    record.append(int(score1) + int(score2))
+            
+            elif op == 'D':
+                if len(record) >= 1:
+                    record.append(2 * int(record[-1]))
+            
+
+            elif op == 'C':
+                if len(record) >= 1:
+                    record.pop()
+        
+
+            print(record)
+            
+        print(record)
+
+        return sum([int(num) for num in record])
+
+        
