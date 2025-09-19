8c7bcbfb160518b8c3307c180ef85747b250d963
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-18 23:22:59 -0700

    Create 3251-MaximumAreaOfLongestDiagonalRectangle

---
+++ 3251-MaximumAreaOfLongestDiagonalRectangle/3251-MaximumAreaOfLongestDiagonalRectangle.py
@@ -0,0 +1,27 @@
+# Last updated: 9/18/2025, 11:22:59 PM
+class Solution(object):
+    def areaOfMaxDiagonal(self, dimensions):
+        """
+        :type dimensions: List[List[int]]
+        :rtype: int
+        """
+        diagonal_length = []
+        max_length = float('-inf')
+        for dimension in dimensions:
+            length = sqrt(dimension[0] * dimension[0] + dimension[1] * dimension[1])
+            diagonal_length.append(length)
+
+            if length > max_length:
+                max_length = length
+
+        area = []
+        
+        for i, diagonal_len in enumerate(diagonal_length):
+            if diagonal_len == max_length:
+                area.append(dimensions[i][0] * dimensions[i][1])
+        
+        return max(area)
+        
+
+
+        
