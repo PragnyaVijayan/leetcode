443d6ae62d60c2403057fd5ace29d93462c80c21
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-18 23:36:18 -0700

    File updated at 9/18/2025, 11:36:18 PM

--- 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.py
+++ 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.py
@@ -1,4 +1,4 @@
-# Last updated: 9/18/2025, 11:35:07 PM
+# Last updated: 9/18/2025, 11:36:18 PM
 class Solution(object):
     def firstPalindrome(self, words):
         """
@@ -6,17 +6,7 @@ class Solution(object):
         :rtype: str
         """
         for word in words:
-            forward_pointer, reverse_pointer = 0, len(word) - 1
-            is_palindrome = True
-
-            while forward_pointer < reverse_pointer:
-                if word[forward_pointer] != word[reverse_pointer]:
-                    is_palindrome = False
-                    break
-                forward_pointer += 1
-                reverse_pointer -= 1
-
-            if is_palindrome:
-                return word
-
+            s = str(word)
+            if s == s[::-1]:
+                return s
         return ""
