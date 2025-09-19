d4dfbe53aed3cfd6ed94e6cf4394b3b49960e68d
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-18 23:23:20 -0700

    Create 1426-FindNUniqueIntegersSumUpToZero

---
+++ 1426-FindNUniqueIntegersSumUpToZero/1426-FindNUniqueIntegersSumUpToZero.py
@@ -0,0 +1,24 @@
+# Last updated: 9/18/2025, 11:23:19 PM
+class Solution(object):
+    def sumZero(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        sum_zero_arr = []
+
+        count = 0
+        num = 1
+        while count <= n - 2:
+            sum_zero_arr.append(num)
+            sum_zero_arr.append(-num)
+            count += 2
+            num += 1
+        
+        if n % 2 == 1:
+            sum_zero_arr.append(0)
+        
+        return sum_zero_arr
+
+
+        
