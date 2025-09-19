9d957a6fbab6e02ad6aabf062a38422afc5b2ebc
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-18 23:41:55 -0700

    File updated at 9/18/2025, 11:41:54 PM

--- 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.py
+++ 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.py
@@ -1,12 +1,29 @@
-# Last updated: 9/18/2025, 11:36:46 PM
+# Last updated: 9/18/2025, 11:41:54 PM
 class Solution(object):
-    def firstPalindrome(self, words):
+
+    def getProduct(self, nums):
+        result = 1
+        for num in nums:
+            result *= num
+        
+        return result
+    
+    def signFunc(self, x):
+        if x > 0: 
+            return 1
+        elif x < 0:
+            return -1
+        else:
+            return 0
+
+
+    def arraySign(self, nums):
         """
-        :type words: List[str]
-        :rtype: str
+        :type nums: List[int]
+        :rtype: int
         """
-        for word in words:
-            s = str(word)
-            if s == s[::-1]:
-                return s
-        return ""
+
+        return self.signFunc(self.getProduct(nums))
+
+
+        
