11b99393eb66dc5c93443f74a981305ee197064c
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-18 23:22:58 -0700

    Create 3515-FindIfDigitGameCanBeWon

---
+++ 3515-FindIfDigitGameCanBeWon/3515-FindIfDigitGameCanBeWon.py
@@ -0,0 +1,21 @@
+# Last updated: 9/18/2025, 11:22:57 PM
+class Solution(object):
+    def canAliceWin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        sum_double_digits = 0
+        sum_single_digits = 0
+
+        for num in nums:
+            if 1 <= num <= 9:
+                sum_single_digits += num
+            elif 10 <= num <= 99:
+                sum_double_digits += num
+
+        #print(sum_single_digits, sum_double_digits)
+        return (sum_double_digits != sum_single_digits)
+
+        
