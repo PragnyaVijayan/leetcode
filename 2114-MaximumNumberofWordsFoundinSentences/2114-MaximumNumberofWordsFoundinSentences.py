d976bc0bfb376be7e30d0f991449d4a6d9ce784d
Pragnya Vijayan <126855665+PragnyaVijayan@users.noreply.github.com>
2025-09-24 23:53:16 -0700

    File updated at 9/24/2025, 11:53:16 PM

--- 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.py
+++ 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.py
@@ -1,15 +1,19 @@
-# Last updated: 9/24/2025, 11:51:31 PM
+# Last updated: 9/24/2025, 11:53:16 PM
 class Solution(object):
-    def numberOfEmployeesWhoMetTarget(self, hours, target):
+    def differenceOfSum(self, nums):
         """
-        :type hours: List[int]
-        :type target: int
+        :type nums: List[int]
         :rtype: int
         """
-        count = 0
-        for hour in hours:
-            if hour >= target:
-                count += 1
 
-        return count
+        element_sum = 0
+        digit_sum = 0
+
+        for num in nums:
+            element_sum += num
+            num = str(num)
+            for char in num:
+                digit_sum += int(char)
+        
+        return abs(element_sum - digit_sum)
         
